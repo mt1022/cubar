9ae12e09517a53ad819d332250810274e8bc6a58
mt1022 <mt1022@users.noreply.github.com>
2021-08-05 13:52:42 +0800

    setup dependency

--- R/codon_info.R
+++ R/codon_info.R
@@ -2,10 +2,10 @@ library(Biostrings)
 library(data.table)
 
 codon_table <- function(id_or_name){
-    codon_table <- getGeneticCode(id_or_name, as.data.frame = TRUE)
-    setDT(codon_table, keep.rownames = 'codon')
-    setnames(codon_table, 'AA', 'aa_code')
-    codon_table[, amino_acid := AMINO_ACID_CODE[GENETIC_CODE]]
+    codon_table <- Biostrings::getGeneticCode(id_or_name, as.data.frame = TRUE)
+    data.table::setDT(codon_table, keep.rownames = 'codon')
+    data.table::setnames(codon_table, 'AA', 'aa_code')
+    codon_table[, amino_acid := Biostrings::AMINO_ACID_CODE[Biostrings::GENETIC_CODE]]
     codon_table[aa_code == '*', amino_acid := '*']
     return(codon_table[, .(aa_code, amino_acid, codon)])
 }

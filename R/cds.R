2004247a5db6f624a354e0700d6a4f208c2aec2d
mt1022 <mt1022@users.noreply.github.com>
2021-08-08 17:17:34 +0800

    initial version of qc_cds

--- R/cds.R
+++ R/cds.R
@@ -1,31 +1,54 @@
 seq_to_codons <- function(seq){
-    codons <- IRanges::Views(seq, start = seq(1, length(seq), 3), width = 3)
+    if(class(seq) != 'DNAString'){
+        seq <- DNAString(seq)
+    }
+    if(length(seq) < 3){
+        stop('Input sequence too short')
+    }
+    codons <- IRanges::Views(seq, start = seq(1, length(seq) - 2, 3), width = 3)
     return(as.character(codons))
 }
 
-#' TODO: min CDS length and subseq operations
-trim_cds <- function(cds, gcid = '1', min_len = 6, check_len = TRUE, check_start = TRUE,
+#' Quality control of CDS
+#'
+#' \code{qc_cds} perform quality control of CDS sequences by filtering some peculiar sequences
+#' and optionally remove start or stop codons.
+#'
+#' @param cds input CDS sequences
+#' @return filtered (and trimmed) CDS, DNAStringSet
+qc_cds <- function(cds, min_len = 6, check_len = TRUE, check_start = TRUE,
                      check_stop = TRUE, check_istop = TRUE,
-                     rm_start = TRUE, rm_stop = TRUE, rm_istop = TRUE){
+                     rm_start = TRUE, rm_stop = TRUE, gcid = '1'){
     gct <- codon_table(gcid)
     stop_codons <- gct[aa_code == '*', codon]
     # if input is RNA sequences, convert to DNA
     if(class(cds) == 'RNAStringSet') cds <- Biostrings::DNAStringSet(cds)
-    cds <- cds[width(cds) >= min_len]
+    cds <- cds[IRanges::width(cds) >= min_len]
+    # CDS length is multiple of 3?
     if(check_len){
-        # CDS length is multiple of 3
-        cds <- cds[(width(cds) %% 3) == 0]
+        cds <- cds[(IRanges::width(cds) %% 3) == 0]
     }
+    # begin with start codon ATG?
     if(check_start){
-        cds <- cds[subseq(cds, 1, 3) == 'ATG']
+        cds <- cds[Biostrings::subseq(cds, 1, 3) == 'ATG']
     }
+    # end with a stop codon
     if(check_stop){
-        cds <- cds[subseq(cds, width(cds) - 2, width(cds)) %in% stop_codons]
+        cds <- cds[Biostrings::subseq(cds, IRanges::width(cds) - 2, IRanges::width(cds)) %in% stop_codons]
     }
+    # no internal stop codons?
     if(check_istop){
-        w_istop <- sapply(subseq(cds, 1, width(cds) - 3), function(x){
+        w_istop <- sapply(Biostrings::subseq(cds, 1, IRanges::width(cds) - 3), function(x){
             any(seq_to_codons(x) %in% stop_codons)
         })
         cds <- cds[!w_istop]
     }
+    # trimming
+    if(rm_start){
+        cds <- Biostrings::subseq(cds, 4, IRanges::width(cds))
+    }
+    if(rm_stop){
+        cds <- Biostrings::subseq(cds, 1, IRanges::width(cds) - 3)
+    }
+    return(cds)
 }
--- R/codon_info.R
+++ R/codon_info.R
@@ -6,3 +6,4 @@ codon_table <- function(gcid = '1'){
     codon_table[aa_code == '*', amino_acid := '*']
     return(codon_table[, .(aa_code, amino_acid, codon)])
 }
+

fc8b404f8dcee311231a798e894543f42f4a7b40
Hong Zhang <mt1022@users.noreply.github.com>
2023-06-24 15:56:06 +0800

    document fly and yeast cds data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' yeast CDS sequences
+#'
+#' CDSs of all protein-coding genes in Saccharomyces_cerevisiae
+#'
+#' @format ## `yeast_cds`
+#' A DNAStringSet of 6600 yeast CDS sequences
+#' @source <https://ftp.ensembl.org/pub/release-107/fasta/saccharomyces_cerevisiae/cds/Saccharomyces_cerevisiae.R64-1-1.cds.all.fa.gz>
+"yeast_cds"
+
+
+#' fly CDS sequences
+#'
+#' CDSs of all protein-coding genes in Drosophila melanogaster
+#'
+#' @format ## `dm6_cds`
+#' A DNAStringSet of 20,476 fly CDS sequences
+#' @source <https://ftp.ensembl.org/pub/release-107/fasta/drosophila_melanogaster/cds/Drosophila_melanogaster.BDGP6.32.cds.all.fa.gz>
+"dm6_cds"

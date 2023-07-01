bb8b5dfc7ffcd7887b9a4c8feb58d653dc0fdecc
Hong Zhang <mt1022@users.noreply.github.com>
2023-07-01 09:54:44 +0800

    doc sequences module

--- R/sequences.R
+++ R/sequences.R
@@ -1,6 +1,6 @@
 #' Convert CDS to codons
 #'
-#' \code{seq_to_codons} convert a coding sequence to a vector of codons
+#' \code{seq_to_codons} converts a coding sequence to a vector of codons
 #'
 #' @param seq DNAString, or an object that can be coerced to a DNAString
 #' @return a character vector of codons
@@ -17,31 +17,43 @@ seq_to_codons <- function(seq){
 
 #' Quality control of CDS
 #'
-#' \code{qc_cds} performs quality control of CDS sequences by filtering some
+#' \code{check_cds} performs quality control of CDS sequences by filtering some
 #' peculiar sequences and optionally remove start or stop codons.
 #'
 #' @param seqs input CDS sequences
-#' @return filtered (and trimmed) CDS, DNAStringSet
-qc_cds <- function(seqs, min_len = 6, check_len = TRUE, check_start = TRUE,
-                     check_stop = TRUE, check_istop = TRUE,
-                     rm_start = TRUE, rm_stop = TRUE, gcid = '1'){
-    gct <- get_codon_table(gcid)
-    stop_codons <- gct[aa_code == '*', codon]
+#' @param min_len minimum CDS length in nt
+#' @param check_len check whether CDS length is divisible by 3
+#' @param check_start check whether CDSs have start codons
+#' @param check_stop check whether CDSs have stop codons
+#' @param check_istop check internal stop codons
+#' @param rm_start whether to remove start codons
+#' @param rm_stop whether to remove stop codons
+#' @param codon_table codon table matching the genetic code of \code{seqs}
+#' @return DNAStringSet of filtered (and trimmed) CDS sequences
+check_cds <- function(seqs, codon_table, min_len = 6, check_len = TRUE,
+                      check_start = TRUE, check_stop = TRUE, check_istop = TRUE,
+                      rm_start = TRUE, rm_stop = TRUE, start_codons = c("ATG")){
+    if(missing(codon_table)){
+        codon_table <- get_codon_table()
+    }
+    stop_codons <- codon_table[aa_code == '*', codon]
     # if input is RNA sequences, convert to DNA
-    if(class(seqs) == 'RNAStringSet') seqs <- Biostrings::DNAStringSet(seqs)
+    if(class(seqs) == 'RNAStringSet'){
+        seqs <- Biostrings::DNAStringSet(seqs)
+    }
     seqs <- seqs[IRanges::width(seqs) >= min_len]
-    # CDS length is multiple of 3?
+    # CDS length is divisible by 3?
     if(check_len){
         seqs <- seqs[(IRanges::width(seqs) %% 3) == 0]
     }
     # begin with start codon ATG?
     if(check_start){
-        seqs <- seqs[Biostrings::subseq(seqs, 1, 3) == 'ATG']
+        seqs <- seqs[as.character(Biostrings::subseq(seqs, 1, 3)) %in% start_codons]
     }
     # end with a stop codon
     if(check_stop){
         x <- IRanges::width(seqs)
-        seqs <- seqs[Biostrings::subseq(seqs, x - 2, x) %in% stop_codons]
+        seqs <- seqs[as.character(Biostrings::subseq(seqs, x - 2, x)) %in% stop_codons]
     }
     # no internal stop codons?
     if(check_istop){
@@ -61,12 +73,13 @@ qc_cds <- function(seqs, min_len = 6, check_len = TRUE, check_start = TRUE,
     return(seqs)
 }
 
+
 #' Count codon frequencies
 #'
-#' \code{count_codons} tabulate all the 64 codons in input CDSs
+#' \code{count_codons} tabulates the occurrences of all the 64 codons in input CDSs
 #'
-#' @param seqs CDSs, DNAStringSet
-#' @return matrix of codon (column) frequencies in each CDS (row)
+#' @param seqs CDS sequences, DNAStringSet
+#' @return matrix of codon (column) frequencies of each CDS (row)
 count_codons <- function(seqs, ...){
     cf <- Biostrings::trinucleotideFrequency(seqs, step = 3, ...)
     rownames(cf) <- names(seqs)

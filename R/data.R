687bc7d331f01604e46245721f21c5ab8ee86b29
Hong Zhang <mt1022@users.noreply.github.com>
2023-07-11 17:56:15 +0800

    add yeast mRNA expression data

--- R/data.R
+++ R/data.R
@@ -7,6 +7,7 @@
 #' @source <https://ftp.ensembl.org/pub/release-107/fasta/saccharomyces_cerevisiae/cds/Saccharomyces_cerevisiae.R64-1-1.cds.all.fa.gz>
 "yeast_cds"
 
+
 #' Half life of yeast mRNAs
 #'
 #' Half life of yeast mRNAs in Saccharomyces_cerevisiae calculated from rRNA-deleted total RNAs by Presnyak et al.
@@ -14,7 +15,19 @@
 #' @format ## `yeast_half_life`
 #' A data.frame with 3888 rows and three columns (gene_id, gene_name, half_life in min)
 #' @source <https://doi.org/10.1016/j.cell.2015.02.029>
+#' @references Presnyak V, Alhusaini N, Chen YH, et al. Codon optimality is a major determinant of mRNA stability. Cell. 2015;160(6):1111-1124. doi:10.1016/j.cell.2015.02.029
 "yeast_half_life"
+
+
+#' yeast mRNA expression levels
+#'
+#' Yeast mRNA FPKM determined from rRNA-depleted (RiboZero) total RNA-Seq libraries. RUN1_0_WT and RUN2_0_WT (0 min after RNA Pol II repression) were averaged and used here.
+#'
+#' @format ## `yeast_exp`
+#' A data.frame with 6717 rows and three columns (gene_id, gene_name, fpkm)
+#' @source <https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE57385&format=file&file=GSE57385%5FRiboZero%5FFPKM%2Etxt%2Egz>
+#' @references Presnyak V, Alhusaini N, Chen YH, et al. Codon optimality is a major determinant of mRNA stability. Cell. 2015;160(6):1111-1124. doi:10.1016/j.cell.2015.02.029
+"yeast_exp"
 
 
 #' amino acids to codons

b04a1dab01e9c94ef7c5b4470e5795f332326d81
Hong Zhang <mt1022@users.noreply.github.com>
2023-07-12 14:19:05 +0800

    update tests

--- tests/testthat/test-count_codons.R
+++ tests/testthat/test-count_codons.R
@@ -1,13 +1,19 @@
-test_that("return is matrix - single", {
-  expect_equal(
-      dim(count_codons(yeast_cds[1])),
-      c(1L, 64L)
-  )
-})
-
-test_that("return is matrix - multiple", {
+test_that("return is matrix", {
+    expect_equal(
+        dim(count_codons(yeast_cds[1])),
+        c(1L, 64L)
+    )
     expect_equal(
         dim(count_codons(yeast_cds)),
         c(6600L, 64L)
+    )
+})
+
+
+test_that("codon frequency sum is correct", {
+    expect_equal(
+        rowSums(count_codons(yeast_cds)),
+        Biostrings::width(yeast_cds)/3,
+        ignore_attr = TRUE
     )
 })

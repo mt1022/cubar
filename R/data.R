ea8db949f1b18928f3d021d4e6ceb2e2882b6ae0
Hong Zhang <mt1022@users.noreply.github.com>
2023-07-13 10:25:45 +0800

    update data documentation

--- R/data.R
+++ R/data.R
@@ -2,9 +2,10 @@
 #'
 #' CDSs of all protein-coding genes in Saccharomyces_cerevisiae
 #'
-#' @format ## `yeast_cds`
-#' A DNAStringSet of 6600 yeast CDS sequences
+#' @format a DNAStringSet of 6600 sequences
 #' @source <https://ftp.ensembl.org/pub/release-107/fasta/saccharomyces_cerevisiae/cds/Saccharomyces_cerevisiae.R64-1-1.cds.all.fa.gz>
+#' @examples
+#' head(yeast_cds)
 "yeast_cds"
 
 
@@ -12,10 +13,16 @@
 #'
 #' Half life of yeast mRNAs in Saccharomyces_cerevisiae calculated from rRNA-deleted total RNAs by Presnyak et al.
 #'
-#' @format ## `yeast_half_life`
-#' A data.frame with 3888 rows and three columns (gene_id, gene_name, half_life in min)
+#' @format a data.frame with 3888 rows and three columns:
+#' \describe{
+#' \item{gene_id}{gene id}
+#' \item{gene_name}{gene name}
+#' \item{half_life}{mRNA half life in minutes}
+#' }
 #' @source <https://doi.org/10.1016/j.cell.2015.02.029>
 #' @references Presnyak V, Alhusaini N, Chen YH, et al. Codon optimality is a major determinant of mRNA stability. Cell. 2015;160(6):1111-1124. doi:10.1016/j.cell.2015.02.029
+#' @examples
+#' head(yeast_half_life)
 "yeast_half_life"
 
 
@@ -23,10 +30,16 @@
 #'
 #' Yeast mRNA FPKM determined from rRNA-depleted (RiboZero) total RNA-Seq libraries. RUN1_0_WT and RUN2_0_WT (0 min after RNA Pol II repression) were averaged and used here.
 #'
-#' @format ## `yeast_exp`
-#' A data.frame with 6717 rows and three columns (gene_id, gene_name, fpkm)
+#' @format a data.frame with 6717 rows and three columns:
+#' \describe{
+#' \item{gene_id}{gene ID}
+#' \item{gene_name}{gene name}
+#' \item{fpkm}{mRNA expression level in Fragments per kilobase per million reads}
+#' }
 #' @source <https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE57385&format=file&file=GSE57385%5FRiboZero%5FFPKM%2Etxt%2Egz>
 #' @references Presnyak V, Alhusaini N, Chen YH, et al. Codon optimality is a major determinant of mRNA stability. Cell. 2015;160(6):1111-1124. doi:10.1016/j.cell.2015.02.029
+#' @examples
+#' head(yeast_exp)
 "yeast_exp"
 
 
@@ -34,10 +47,11 @@
 #'
 #' Yeast tRNA gene copy numbers (GCN) by anticodon obtained from gtRNAdb.
 #'
-#' @format ## `yeast_trna_gcn`
-#' A named (anticodon) vector with a length of 41 anticodons.
+#' @format a named vector with a length of 41. Value names are anticodons.
 #' @source <http://gtrnadb.ucsc.edu/genomes/eukaryota/Scere3/sacCer3-mature-tRNAs.fa>
 #' @references Chan, P.P. & Lowe, T.M. (2016) GtRNAdb 2.0: an expanded database of transfer RNA genes identified in complete and draft genomes. Nucl. Acids Res. 44(Database issue):D184-D189.Chan, P.P. & Lowe, T.M. (2016) GtRNAdb 2.0: an expanded database of transfer RNA genes identified in complete and draft genomes. Nucl. Acids Res. 44(Database issue):D184-D189.
+#' @examples
+#' yeast_trna_gcn
 "yeast_trna_gcn"
 
 
@@ -45,7 +59,13 @@
 #'
 #'  A data.frame of mapping from amino acids to codons
 #'
-#' @format ## `aa2codon`
-#' data.frame with two columns: amino_acid, and codons.
+#' @format a data.frame with two columns: amino_acid, and codon.
+#' \describe{
+#' \item{amino_acid}{amino acid corresponding to the codon}
+#' \item{codon}{codon identity}
+#' }
 #' @source It is actually the standard genetic code.
+#' @examples
+#' aa2codon
+#'
 "aa2codon"

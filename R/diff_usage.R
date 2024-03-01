e0a3b7f7a206d095342faa039d4887d546b573be
ZiBuZiBu <zibu997135794@163.com>
2024-03-01 22:22:55 +0800

    Rename get_diffUsage to compare_codon_usage and standard format

--- R/diff_usage.R
+++ R/diff_usage.R
@@ -1,19 +1,22 @@
+#' Compare the codons that differ between two sets of sequences
+#'
+#' \code{get_diffUsage} screens out the codons with usage preferences in the two sets of sequences.
+#'
+#' @param cds_list_one a DNA sequence to analyze.
+#' @param cds_list_two another DNA sequence to analyze.
+#' @param gcid a parameter in get_codon_table() to get the required codon table for mapping codons to amino acids.
+#' @returns a data frame containing the computed metrics.
+#' @examples compares two sets of genes in yeast
+#' result <- get_diffUsage (yeast_cds[1:50], yeast_cds[51:100], gcid = "1" )
+#' print(result)
+
 library(Biostrings)
 library(dplyr)
 
-#' \code{compare_codon_usage} Screen out the codons with usage preferences in the two sets of sequences.
-#' @param cds_list_one: A DNA sequence to analyze.
-#' @param cds_list_two: Another DNA sequence to analyze.
-#' @param gcid: A parameter in get_codon_table() to get the required codon table for mapping codons to amino acids.
-#' @returns A data frame containing the computed metrics.
-#' @examples Comparing two sets of genes in yeast
-#' result <- compare_codon_usage(yeast_cds[1:50], yeast_cds[51:100], gcid = "1" )
-#' print(result)
+get_diffUsage <- function(cds_list_one, cds_list_two, gcid = NULL) {
 
-compare_codon_usage <- function(cds_list_one, cds_list_two, gcid = NULL) {
-  
   yeast_cds_qc_one <- check_cds(cds_list_one)
-  yeast_cds_qc_two <- check_cds(cds_list_two) 
+  yeast_cds_qc_two <- check_cds(cds_list_two)
 
   # Count the total number of each codon
   yeast_cf_one <- count_codons(yeast_cds_qc_one)
@@ -35,7 +38,7 @@ compare_codon_usage <- function(cds_list_one, cds_list_two, gcid = NULL) {
   stop_codons <- c("TAA", "TAG", "TGA")
   single_codon_amino_acids <- c("TGG", "ATG")
   data_tr <- data_df[!(data_df$codon %in% c(stop_codons, single_codon_amino_acids)),]
-  
+
   # Fisher test
   by_amino_acid <- split(data_tr, data_tr$amino_acid)
   results <- data.frame(AminoAcid = character(),
@@ -44,7 +47,7 @@ compare_codon_usage <- function(cds_list_one, cds_list_two, gcid = NULL) {
                         Codon_subfam = character(),
                         P_Value = numeric(),
                         stringsAsFactors = FALSE)
-  
+
   for (amino_acid in names(by_amino_acid)) {
     codon_group <- by_amino_acid[[amino_acid]]
 
@@ -66,7 +69,7 @@ compare_codon_usage <- function(cds_list_one, cds_list_two, gcid = NULL) {
         next # Skip this codon
       }
       fisher_result <- fisher.test(contingency_table)
-      
+
       # Append the results
       results <- rbind(results, data.frame(AminoAcid = amino_acid,
                                            Codon = as.character(row[['codon']]),
@@ -76,11 +79,10 @@ compare_codon_usage <- function(cds_list_one, cds_list_two, gcid = NULL) {
                                            stringsAsFactors = FALSE))
       }
     }
-  
+
   # Apply BH correction
   results$P_Adjusted_BH <- p.adjust(results$P_Value, method = "BH")
 
   return(results)
 }
-
 

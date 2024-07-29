69b2b651306d0f7a638f39e012ac6b79d6a86e87
Hong Zhang <mt1022@users.noreply.github.com>
2024-07-29 12:44:40 +0800

    extend codon optimizaiton utility
    - support optimization at both amino_acid (family) or subfamily level;
    - more flexible optimal codon specification;
    - allow custom codon tables;

--- tests/testthat/test-codon_optimize.R
+++ tests/testthat/test-codon_optimize.R
@@ -1,6 +1,9 @@
 test_that("optimization works", {
     optimal_codons <- data.table::data.table(
-        aa_code = rep('K', 2), codon = c('AAA', 'AAG'), coef = c(-0.5, 0.5))
+        aa_code = rep('K', 2),
+        codon = c('AAA', 'AAG'),
+        coef = c(0.5, -0.5),
+        subfam = c('Lys_AA', 'Lys_AA'))
     seq <- codon_optimize('AAGAAA', optimal_codons)
     expect_equal(as.character(seq), 'AAAAAA')
 })
